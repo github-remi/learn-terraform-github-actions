ae740e105f2ad1fbdc72a8f4edcc4bc4ff34df05
github-remi <94445121+github-remi@users.noreply.github.com>
2024-09-24 15:05:52 -0500

    Revert "Revert "Point backend to correct TFC org and workspace""

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos78"
+    organization = "ACG-Terraform-Demos419"
 
     workspaces {
       name = "demo-github-actions"
